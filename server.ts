0677e5dc1c9121d890050570d3c420d4a7ff07b1
Jorge Saa <piojo.sistematico@gmail.com>
2024-06-21 20:35:39 -0400

    added some middleware and the acccess token to a cookie

--- server.ts
+++ server.ts
@@ -1,15 +1,30 @@
 import express from "express";
 import { createUser, login } from "./database/db";
 import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  const token = req.cookies.access_token;
+  let data = null;
+  req.session = { user: null };
+
+  try {
+    const data = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.session = data;
+  } catch {}
+  next();
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  const { user } = req.session;
+  res.status(200).json({ user });
 });
 
 app.post("/login", async (req, res) => {
@@ -22,7 +37,13 @@ app.post("/login", async (req, res) => {
       process.env.JWT_SECRET_KEY,
       { expiresIn: "1h" }
     );
-    res.send({ user: info.userName, token: token });
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        sameSite: true,
+        maxAge: 1000 * 60 * 60,
+      })
+      .send({ user: info.userName, token: token });
   } catch (error) {
     if (error instanceof Error) {
       res.status(400).json({ message: error.message });
@@ -39,17 +60,27 @@ app.post("/register", async (req, res) => {
     res.send({ id: id });
   } catch (error) {
     if (error instanceof Error) {
-      res.status(400).json({ message: error.message });
+      res.status(400).json({ message: "Unauthorized access" });
     }
   }
 });
 
 app.post("/logout", (req, res) => {
-  res.send("Logout");
+  res.clearCookie("access_token").json({ message: "Good bye" });
 });
 
 app.post("/protected", (req, res) => {
-  res.send("Protected");
+  const token = req.cookies.access_token;
+  if (!token) res.status(400).json({ message: "error.message" });
+
+  try {
+    const data = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    res.send({ data });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).json({ message: "Unauthorized access" });
+    }
+  }
 });
 
 app.listen(PORT, () => {
